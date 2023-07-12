389fc5dbcc99b42eced023fba7ed305454e30af7
Timo <Timo.evers@gmail.com>
2023-07-12 13:47:48 +0200

    Functie fooi_pp toegevoegd aan helper.py

--- helper.py
+++ helper.py
@@ -5,3 +5,8 @@ def decoreer(tekst=""):
   print(f"* {tekst} *")
   print(lengte * "*")
   print()
+
+def fooi_pp(bedrag, personen):
+  bedrag_pp = bedrag / personen
+  return f"Het bedrag per persoon is {bedrag_pp} euro"
+  
